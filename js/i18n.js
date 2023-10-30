fed663f1629fdb2ef32462577eefea1dbd13a5de
Benjamin Brodwolf <benjamin.brodwolf@youengineering.ch>
2023-10-30 16:19:00 +0100

    add i18n translation script

---
+++ js/i18n.js
@@ -0,0 +1,59 @@
+const availableLocales = ['de', 'fr', 'it'];
+const defaultLanguage = 'de'
+
+const navigatorLanguage = (window.navigator.userLanguage || window.navigator.language).substring(0, 2);
+let pageLanguage = defaultLanguage;
+if (availableLocales.includes(navigatorLanguage)) {
+    pageLanguage = navigatorLanguage;
+}
+
+
+
+console.log(pageLanguage)
+
+const locales = {
+    de: {
+        "start": {
+            "title": "Deutscher Titel",
+            "subtitle": "Deutscher Subtitle",
+        },
+    },
+    fr: {
+        "start": {
+            "title": "Französischer Titel",
+            "subtitle": "Französischer Subtitle",
+        },
+    },
+};
+
+const elements = document.querySelectorAll('[data-i18n]');
+
+
+const translate = () => {
+    const json = locales[pageLanguage];
+    elements.forEach((element, index) => {
+        const key = element.getAttribute('data-i18n');
+        element.innerHTML = key.split('.').reduce((obj, i) => (obj ? obj[i] : null), json);
+    });
+}
+const changeLanguage = language => {
+    if (language === pageLanguage) return;
+    pageLanguage = language;
+    translate()
+}
+
+if (pageLanguage !== defaultLanguage) {
+    translate()
+}
+
+const activeLangClass = 'activeLang';
+
+const langElement = document.querySelectorAll('.header-languages span');
+langElement.forEach(langEle => langEle.addEventListener('click', () => {
+        langElement.forEach(l => l.classList.remove(activeLangClass))
+        langEle.classList.add(activeLangClass);
+        changeLanguage(langEle.dataset.lang);
+    }
+))
+
+document.querySelector(`[data-lang="${pageLanguage}"]`).classList.add(activeLangClass);
