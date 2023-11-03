af9d60838141a4c3d0ddc99d3ab532a299135380
Benjamin Brodwolf <benjamin.brodwolf@youengineering.ch>
2023-11-04 00:46:19 +0100

    education content display WIP

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@ let turnaroundToggle = false;
 const turnaroundElement = document.querySelector('.turnaround');
 setInterval(() => {
     turnaroundElement.style.transform = turnaroundToggle ? 'rotateY(0deg)' : 'rotateY(180deg)';
-        turnaroundToggle = !turnaroundToggle;
-    }, 3000)
+    turnaroundToggle = !turnaroundToggle;
+}, 3000)
+
+
+const edus = ['efz', 'eba', 'bam']
+const eduParents = document.querySelectorAll('.efz, .eba, .bam');
+console.log(eduParents)
+const buttons = document.querySelectorAll('.educationChoices button');
+console.log(buttons)
+
+const contents = document.querySelectorAll('.efzContent, .ebaContent, .bamContent')
+console.log(contents)
+
+buttons.forEach((btn,index) =>
+    btn.addEventListener('click', () => {
+        eduParents.forEach(e => e.classList.remove('active'));
+        eduParents[index].classList.add('active');
+        contents.forEach(c => c.classList.remove('active'));
+        contents[index].classList.add('active');
+    }))
